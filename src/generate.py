59f68ccf67a637c1923ee4d00bb1a21f4d1679b0
Kelby Hubbard <krhubbard2@alaska.edu>
2022-02-13 23:21:43 -0900

    Base functionality implemented.

---
+++ src/generate.py
@@ -0,0 +1,33 @@
+# Kelby Hubbard
+# Started: 2022-02-13
+# Updated: 
+# generate.py
+#
+# Password generator script
+
+import string
+import random
+
+def random_password(lower, upper, symbol, length):
+    characters = []
+    if (lower == True):
+        characters += list(string.ascii_lowercase)
+    if (upper == True):
+        characters += list(string.ascii_uppercase)
+    if (symbol == True):
+        characters += list("!@#$%^&*()")
+
+    # Shuffle all characters
+    random.shuffle(characters)
+
+    # Add random characters to password
+    passwordList = []
+    for i in range (length):
+        passwordList.append(random.choice(characters))
+    
+    # Shuffle password again
+    random.shuffle(passwordList)
+    
+    password = ''.join(passwordList)
+    
+    return password
--- src/visual.py
+++ src/visual.py
@@ -7,6 +7,7 @@
 
 # GUI for PyPasswordGenerator
 
+from .generate import random_password
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
@@ -18,6 +19,7 @@ from PyQt5.QtWidgets import (
     QLineEdit,
     QSlider,
     QLabel,
+    QVBoxLayout,
 )
 
 # Main Window
@@ -38,15 +40,15 @@ class Window(QMainWindow):
 
         # Line edit
         self.lineEdit = QLineEdit()
-
-        # Generate button
-        self.generateButton = QPushButton('Generate')
+        self.lineEdit.setReadOnly(True)
 
         # Check boxes
         self.lowercaseCheckBox = QCheckBox('Lowercase (abc)')
-        # lowercaseCheckBox.setChecked(self, True)
+        self.lowercaseCheckBox.setChecked(True)
         self.uppercaseCheckBox = QCheckBox('Uppercase (ABC)')
+        self.uppercaseCheckBox.setChecked(True)
         self.symbolCheckBox = QCheckBox('Symbols (!@#$%^&*(){}')
+        self.symbolCheckBox.setChecked(True)
 
         # Length slider
         self.lengthSlider = QSlider(Qt.Horizontal)
@@ -56,8 +58,17 @@ class Window(QMainWindow):
         value = self.lengthSlider.value()
 
         # Slider value label
-        self.label = QLabel('Length: ' + str(value), self)
-        self.label.setGeometry(515, 125, 200, 60)        
+        self.label = QLabel('Length: ' + str(value), self)  
+        self.label.setAlignment(Qt.AlignCenter)
+
+        # Slider and slider value sublayout
+        self.subLayout = QVBoxLayout()
+        self.subLayout.addWidget(self.lengthSlider)
+        self.subLayout.addWidget(self.label)
+
+        # Generate button
+        self.generateButton = QPushButton('Generate')
+        self.generateButton.clicked.connect(lambda: self.setLineEditText(random_password(self.lowercaseCheckBox.isChecked(), self.uppercaseCheckBox.isChecked(), self.symbolCheckBox.isChecked(), self.lengthSlider.value())))
 
         # Layout
         self.layout.addWidget(self.lineEdit, 0, 0, 1, 3)
@@ -65,8 +76,12 @@ class Window(QMainWindow):
         self.layout.addWidget(self.lowercaseCheckBox, 1, 0)
         self.layout.addWidget(self.uppercaseCheckBox, 1, 1)
         self.layout.addWidget(self.symbolCheckBox, 1, 2)
-        self.layout.addWidget(self.lengthSlider, 1, 3)
+        self.layout.addLayout(self.subLayout, 1, 3)
 
     # Update slider label value
     def updateLabel(self, value):
         self.label.setText('Length: ' + str(value))
+
+    # Generate Password
+    def setLineEditText(self, string):
+        self.lineEdit.setText(string)
